47a151bb22f5f8c01f3b8d8946802bf1cbfdc39b
nastu <nastushik92@bk.ru>
2022-08-31 18:21:53 +0300

    get

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] weights ={90,91,93,92,85,87,88,89,0,0,0,0};
+        weights[0] = 90;
+        int janiaryWeight=weights[0];
+        System.out.println(janiaryWeight);
+        System.out.println(weights[0]);
+        System.out.println(weights[4]);
+        int january=0;
+        System.out.println(weights[january]);
+        for (int i =0;i>weights.length;i++){
+            System.out.println(weights[i]);
+        }
+    }
+}
